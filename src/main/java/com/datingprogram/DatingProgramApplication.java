a230cc414e29ff605c9d003ac058a249c2abdfdc
hoseung <meteorproject529@gmail.com>
2025-05-09 01:07:10 +0900

    #1 - 스프링 프로젝트 시작
    스프링 부트 프로젝트를 시작함
    - lombok
    - spring web
    - spring devtools
    - spring actuator
    gitignore 설정
    
    This closes #1

---
+++ src/main/java/com/datingprogram/DatingProgramApplication.java
@@ -0,0 +1,13 @@
+package com.datingprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatingProgramApplication.class, args);
+    }
+
+}
